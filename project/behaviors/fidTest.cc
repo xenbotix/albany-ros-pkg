007fb006ac046e1c59ffdadc004087c38e65c452
mikef <mikef@48eb5912-6fe2-3d64-4d7e-704b7dbcc518>
2010-03-29 16:06:15 +0000

    Simple Arm updates

--- project/behaviors/fidTest.cc
+++ project/behaviors/fidTest.cc
@@ -2,32 +2,31 @@
 #ifndef INCLUDED_FIDTEST_h_
 #define INCLUDED_FIDTEST_h_
 
-#include "Shared/RobotInfo.h"
-#include "Behaviors/StateMachine.h"
+#include "Behaviors/BehaviorBase.h"
+#include "Events/EventRouter.h"
 #include "Events/TextMsgEvent.h"
 
 //! DESCRIPTION
-class fidtest : public BehaviorBase {
+class fidTest : public BehaviorBase {
 
 public:
 	//! default constructor, use type name as instance name
-	fidtest():BehaviorBase("fidtest"){}
+	fidTest():BehaviorBase("fidTest"){}
 
     // Start up listeners for timers, sensors, etc
     virtual void DoStart() {
         BehaviorBase::DoStart();
-
 	    erouter->addListener(this,EventBase::textmsgEGID);
     }
 
     virtual void processEvent(const EventBase &event){
 	    if(event.getGeneratorID() == EventBase::textmsgEGID){
             const TextMsgEvent &txtev=dynamic_cast<const TextMsgEvent&>(event);
-            cout << txtev.getText() << endl;
+            std::cout << txtev.getText() << std::endl;
         }
     }
 
-	static std::string getClassDescription() { return "Listen for FID"; }
+	static std::string getClassDescription() { return "Listen for FID recognition messages"; }
 	virtual std::string getDescription() const { return getClassDescription(); }
 
 	// ****************************
@@ -41,10 +40,10 @@ private:
 	// will result if you inadvertantly cause a call to either (which is probably
 	// a good thing, unless you really intended to copy/assign a behavior, in
 	// which case simply provide implementations for the functions)
-	fidtest(const fidtest&); //!< don't call (copy constructor)
-	fidtest& operator=(const fidtest&); //!< don't call (assignment operator)
+	fidTest(const fidTest&); //!< don't call (copy constructor)
+	fidTest& operator=(const fidTest&); //!< don't call (assignment operator)
 };
 
-REGISTER_BEHAVIOR(fidtest);
+REGISTER_BEHAVIOR(fidTest);
 
 #endif
---
+++ project/behaviors/simpleArmTest.cc
@@ -0,0 +1,76 @@
+//-*-c++-*-
+#ifdef TGT_NELSON
+
+/* Walk With Me! - A Nelson Demo
+ * Copyright 2010 Michael E. Ferguson - SUNY Albany Robotics
+ */
+
+#ifndef INCLUDED_SIMPLE_ARM_TEST_h_
+#define INCLUDED_SIMPLE_ARM_TEST_h_
+
+#include "Behaviors/BehaviorBase.h"
+#include "Events/EventRouter.h"
+#include "Motion/PIDMC.h"
+#include "Motion/SimpleArmMC.h"
+#include "IPC/SharedObject.h"
+#include "Motion/MMAccessor.h"
+#include "IPC/SharedObject.h"
+#include "Motion/MotionPtr.h"
+
+/* We create an ArmMC for the right arm, and move the left arm for voodoo control.  */
+class simpleArmTest : public BehaviorBase {
+
+public:
+    //! default constructor, use type name as instance name
+    simpleArmTest():BehaviorBase("SimpleArmMC Test Program"), pid(SharedObject<PIDMC>(1)),
+    arm_id(motman->addPersistentMotion(SharedObject<SimpleArmMC>(NelsonInfo::RightArmOffset))) {}
+
+    // Start up listeners for timers, sensors, etc
+    virtual void DoStart() {
+        BehaviorBase::DoStart();
+        std::cout << "Simple Arm Test: starting..." << std::endl;
+        // Turn off torque to our left arm!
+        for(unsigned int i=0; i< NelsonInfo::JointsPerArm; i++){
+            pid->setJointPowerLevel(NelsonInfo::LeftArmOffset+i,0);
+        }
+        addMotion(pid,PERSISTENT,MotionManager::kHighPriority);
+        // Listen for a 30Hz update rate
+        erouter->addListener(this,EventBase::sensorEGID);
+    }
+
+    virtual void processEvent(const EventBase &event){
+        volatile float pitch = state->outputs[NelsonInfo::LeftArmOffset+NelsonInfo::ArmShoulderPitchOffset];
+        volatile float shoulder = state->outputs[NelsonInfo::LeftArmOffset+NelsonInfo::ArmShoulderOffset];
+        volatile float elbow = state->outputs[NelsonInfo::LeftArmOffset+NelsonInfo::ArmElbowOffset];
+        MMAccessor<SimpleArmMC> arm(arm_id);
+        arm.mc()->setJoints(pitch, shoulder, elbow, 0);
+    }
+
+    virtual void DoStop(){
+        motman->removeMotion(arm_id);
+        removeMotion(pid);
+        // this "one-shot" version of doing things will restore the PIDs on our way out
+        // note the explicit call to motman with a naked SharedObject instead of using
+        // BehaviorBase::addMotion or MotionPtr Ñ otherwise this would be immediately
+        // stopped again as the behavior exits and not have time to do anything
+        motman->addPrunableMotion(SharedObject<PIDMC>(1));
+        std::cout << "Simple Arm Test: terminated." << std::endl;
+        BehaviorBase::DoStop();
+    }
+
+    static std::string getClassDescription() { return "SimpleArmMC Test Program"; }
+    virtual std::string getDescription() const { return getClassDescription(); }
+
+protected:
+    MotionPtr<PIDMC> pid;           // need to disable left arm, derived from RecordRange.cc/RelaxBehavior.cc
+    MotionManager::MC_ID arm_id; 
+
+private:
+    simpleArmTest(const simpleArmTest&);  // don't call (copy constructor)
+    simpleArmTest& operator=(const simpleArmTest&); // don't call (assignment operator)
+};
+
+REGISTER_BEHAVIOR(simpleArmTest);
+
+#endif // INCLUDED_SIMPLE_ARM_MC_h_
+#endif // TGT_NELSON
