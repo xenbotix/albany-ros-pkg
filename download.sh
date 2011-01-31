bf7897ba137bee9e44e7b62a400b1b301f2213b8
MFergs7 <MFergs7@48eb5912-6fe2-3d64-4d7e-704b7dbcc518>
2011-01-31 20:01:20 +0000

    clean up presentation of download.sh

--- download.sh
+++ download.sh
@@ -19,6 +19,6 @@ svn co https://bosch-ros-pkg.svn.sourceforge.net/svnroot/bosch-ros-pkg/tags/stac
 #echo 'export ROS_PACKAGE_PATH=~/ros:${ROS_PACKAGE_PATH}' >> ~/.bashrc
 
 echo "You should add the following lines to your ~/.bashrc:"
-echo ". /opt/ros/<distro>/setup.sh where <distro> is cturtle or unstable"
-echo "export ROS_PACKAGE_PATH=~/ros:${ROS_PACKAGE_PATH}"
+echo "  . /opt/ros/<distro>/setup.sh where <distro> is cturtle or unstable"
+echo "  export ROS_PACKAGE_PATH=~/ros:${ROS_PACKAGE_PATH}"
 echo "and then 'source ~/.bashrc' and run install.sh"
